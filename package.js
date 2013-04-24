24274335e677469a6af44f95c3565a93e1e98b9b
Miha Čančula <miha@noughmad.eu>
2013-04-24 19:12:56 +0200

    Include all js files

--- package.js
+++ package.js
@@ -5,8 +5,21 @@ Package.describe({
 Package.on_use(function (api) {
   api.use('jquery', 'client');
   
-  api.add_files('js/foundation/foundation.topbar.js');
+  api.add_files('js/foundation/foundation.js', 'client');
+  api.add_files('js/foundation/foundation.alerts.js', 'client');
+  api.add_files('js/foundation/foundation.clearing.js', 'client');
+  api.add_files('js/foundation/foundation.cookie.js', 'client');
+  api.add_files('js/foundation/foundation.dropdown.js', 'client');
+  api.add_files('js/foundation/foundation.forms.js', 'client');
+  api.add_files('js/foundation/foundation.joyride.js', 'client');
+  api.add_files('js/foundation/foundation.magellan.js', 'client');
+  api.add_files('js/foundation/foundation.orbit.js', 'client');
+  api.add_files('js/foundation/foundation.placeholder.js', 'client');
+  api.add_files('js/foundation/foundation.reveal.js', 'client');
+  api.add_files('js/foundation/foundation.section.js', 'client');
+  api.add_files('js/foundation/foundation.tooltips.js', 'client');
+  api.add_files('js/foundation/foundation.topbar.js', 'client');
   
-  api.add_files('js/foundation.min.js', 'client');
-  api.add_files('css/foundation.min.css', 'client');
+  api.add_files('css/normalize.css', 'client');
+  api.add_files('css/foundation.css', 'client');
 });
