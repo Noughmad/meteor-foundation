5eb8f73da759c4961f9c6018c0e194193577b2ef
Miha Čančula <miha@noughmad.eu>
2013-04-24 19:07:23 +0200

    Import Foundation and package files

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "The most advanced responsive front-end framework in the world."
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+  
+  api.add_files('js/foundation.min.js', 'client');
+  api.add_files('css/foundation.min.css', 'client');
+});
